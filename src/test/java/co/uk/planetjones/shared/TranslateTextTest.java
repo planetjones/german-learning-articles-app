857ee148be9d55fe58d2993c36ebc046fc80deef
Jonathan Jones <jonathanjones@Jonathans-iMac.local>
2020-10-11 13:22:48 +0200

    playing with github actions and ignoring test that needs GCP credentials

--- src/test/java/co/uk/planetjones/shared/TranslateTextTest.java
+++ src/test/java/co/uk/planetjones/shared/TranslateTextTest.java
@@ -3,11 +3,14 @@ package co.uk.planetjones.shared;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class TranslateTextTest {
     
     @Test
+    @Disabled
     public void smokeTest() throws Exception {
         TranslateText.translateText();
     }
